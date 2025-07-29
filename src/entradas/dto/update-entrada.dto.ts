fa33cf92b92f479a6577d7d44e324ebbd8ea183e
Abejandroo <alejandro7521x@gmail.com>
2025-07-29 08:57:03 -0600

    Service de entrradas

--- src/entradas/dto/update-entrada.dto.ts
+++ src/entradas/dto/update-entrada.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateEntradaDto } from './create-entrada.dto';
+import { IsInt, IsOptional } from 'class-validator';
 
-export class UpdateEntradaDto extends PartialType(CreateEntradaDto) {}
+
+export class UpdateEntradaDto {
+    @IsInt()
+    @IsOptional()
+    alumnoId?: number;
+}
